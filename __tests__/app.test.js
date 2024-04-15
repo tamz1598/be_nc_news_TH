37244e3063cf64243bf6d44d3ff7f075d7d78c97
Tamya Hussain <tamyahussain@Tamyas-MacBook-Pro.local>
2024-04-15 12:45:56 +0100

    task 2: returned topics and added error handling

---
+++ __tests__/app.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('../db/app');
+const db = require('../db/connection');
+
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data');
+
+beforeEach(() => {
+  return seed(data);
+})
+
+afterAll(() => {
+  return db.end();
+})
+
+
+describe("NC_NEWS", () => {
+    // get /api/topics
+    describe('/api/topics', () => {
+        test('GET 200: Responds with an array of topic object containing the following properties: slug and description.', () => {
+            return request(app)
+              .get('/api/topics')
+              .expect(200)
+              .then(({ body }) => {
+                const { topics } = body;
+                expect(topics).toHaveLength(3);
+      
+                topics.forEach(topic => {
+                  expect(typeof topic.description).toBe('string');
+                  expect(typeof topic.slug).toBe('string');
+                });
+            });
+        });
+
+        test("GET 404: Responds with an error if passed an invalid sort_by parameter.", () => {
+            return request(app)
+              .get('/api/topics!')
+              .expect(404)
+              .then(({ body }) => {
+                const { message } = body; 
+                  expect(message).toBe('endpoint not found');
+            });
+          });
+    });
+});
--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+//  handle using the correct environment variables here
+
+const ENV = process.env.NODE_ENV || 'development';
+const pathToEnvFile = `${__dirname}/../.env.${ENV}`;
+
+require('dotenv').config({ path: pathToEnvFile });
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
