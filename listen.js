fe4af1e2e488c9f074db0c085fac6880a23247a7
Tamya Hussain <tamyahussain@Tamyas-MacBook-Pro.local>
2024-04-18 15:42:57 +0100

    listen file

--- listen.js
+++ listen.js
@@ -2,5 +2,5 @@ const app = require('./db/app.js');
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-  console.log("listening on ${PORT}");
+  console.log(`listening on ${PORT}`);
 });
