d3077ab144b5e7b02e2bfda22fe5b24de2af1fb5
Tamya Hussain <tamyahussain@Tamyas-MacBook-Pro.local>
2024-05-03 16:12:08 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api');
+const cors = require('cors');
 
 // connect to controller
 const { postCommentsByArticleId, patchArticleByArticleId, patchCommentByCommentId, postArticlesByArticleId} = require('./controller/app.controller');
 
 app.use('/api', apiRouter);
 app.use(express.json());
+app.use(cors());
 
 // endpoints
 
